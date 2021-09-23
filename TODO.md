6666a1093c2675f98b9010df7c6402420c6e9e17
Kaninor <dariushroouhifard@gmail.com>
2021-09-23 22:15:45 +0330

    more TODOs and removing admin's email pass in admin view

--- TODO.md
+++ TODO.md
@@ -1,7 +1,12 @@
 # Todo
 
--   [ ] Search box
 -   [x] Add, Edit view
 -   [ ] Settings view
 -   [ ] Reports view
 -   [x] new controller for add and edit
+-   [x] JQuery for add and edit
+-   [ ] JQuery for increament and decreament
+-   [ ] Data validation
+-   [ ] Changing the product table structure by using migrations
+-   [ ] Maybe making the web app more secure instead of using GET in adding new product we should use POST
+-   [ ] We can remove created at and updated at culomn in the dashboard table and add a new action called INFO and showing some details in there
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,31 +1,31 @@
 @extends('layout2')
 
 @section('header')
-    <title>Admins</title>
-    <link rel="icon" href="https://banner2.cleanpng.com/20190730/ryw/kisspng-icon-design-admin-settings-male-icon-free-download-png-and-5d4039eace2968.0055730815644902188444.jpg">
-    <link rel="stylesheet" href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css">
-    <style>
-    body{
+<title>Admins</title>
+<link rel="icon" href="https://banner2.cleanpng.com/20190730/ryw/kisspng-icon-design-admin-settings-male-icon-free-download-png-and-5d4039eace2968.0055730815644902188444.jpg">
+<link rel="stylesheet" href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css">
+<style>
+    body {
         font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     }
-    li{
+
+    li {
         font-size: 18px;
     }
-    ul{
+
+    ul {
         font-size: 16px;
         line-height: 9px;
     }
-    </style>
+</style>
 @stop
 
 @section('body')
-    @foreach ($admins as $admin)
-        <li>{{ $admin->id }}</li>
-        <ul>{{ $admin->firstName }}</ul>
-        <ul>{{ $admin->lastName }}</ul>
-        <ul>{{ $admin->age }}</ul>
-        <ul>{{ $admin->email }}</ul>
-        <ul>{{ $admin->password }}</ul>
-        <br>
-    @endforeach
+@foreach ($admins as $admin)
+<li>{{ $admin->id }}</li>
+<ul>{{ $admin->firstName }}</ul>
+<ul>{{ $admin->lastName }}</ul>
+<ul>{{ $admin->age }}</ul>
+<br>
+@endforeach
 @stop
