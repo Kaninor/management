b90a7bc96cc0623b98cd642f953e6dbb64249a83
Kaninor <dariushroouhifard@gmail.com>
2021-09-25 21:11:57 +0330

    adding meta tag for charset and viewport

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,8 @@
 
 <head>
   <link rel="stylesheet" href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @yield('header')
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&family=Righteous&family=Signika+Negative:wght@600;700&display=swap');
